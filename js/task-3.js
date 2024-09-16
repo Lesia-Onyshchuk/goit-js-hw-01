c85b2cb3af75847fb5e10f5d629a909e77ea492d
lesia_onyshchuk <lkane443@gmail.com>
2024-09-16 11:53:29 +0300

    t3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,9 @@
 function getElementWidth(content, padding, border) {
-  padding = 2 * padding;
-  border = 2 * border;
-  return Number.parseFloat(content + padding + border);
+  return (
+    Number.parseFloat(content) +
+    2 * Number.parseFloat(padding) +
+    2 * Number.parseFloat(border)
+  );
 }
 
 console.log(getElementWidth('50px', '8px', '4px')); // 74
